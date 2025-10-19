3e0dacc8dfd2c2ab22e7f1eaf080d77adc038318
sbnm007 <sibin.georgge@gmail.com>
2025-10-19 01:56:27 +0100

    AKS Provisioned

--- main.tf
+++ main.tf
@@ -11,7 +11,7 @@ terraform {
 
 provider "azurerm" {
   features {}
-  
+
   # Disable automatic resource provider registration for limited permission accounts
   skip_provider_registration = true
 }
@@ -86,7 +86,34 @@ resource "azurerm_subnet_network_security_group_association" "aks" {
 
 
 
+# AKS Cluster
+resource "azurerm_kubernetes_cluster" "bookinfo" {
+  name                = var.cluster_name
+  location            = azurerm_resource_group.bookinfo.location
+  resource_group_name = azurerm_resource_group.bookinfo.name
+  dns_prefix          = "${var.cluster_name}-dns"
+  kubernetes_version  = var.kubernetes_version
 
+  default_node_pool {
+    name           = "default"
+    node_count     = var.node_count
+    vm_size        = var.node_vm_size
+    vnet_subnet_id = azurerm_subnet.aks.id
+    type           = "VirtualMachineScaleSets"
+  }
+
+  identity {
+    type = "SystemAssigned"
+  }
+
+  network_profile {
+    network_plugin = "azure"
+    service_cidr   = "10.1.0.0/16" # virtual ips for services
+    dns_service_ip = "10.1.0.10"
+  }
+
+  tags = var.tags
+}
 
 
 
@@ -117,4 +144,42 @@ output "vnet_id" {
 output "subnet_id" {
   description = "ID of the AKS subnet"
   value       = azurerm_subnet.aks.id
+}
+
+output "client_certificate" {
+  description = "Client certificate for kubectl"
+  value       = azurerm_kubernetes_cluster.bookinfo.kube_config[0].client_certificate
+  sensitive   = true
+}
+
+output "kube_config" {
+  description = "Raw kubeconfig for the AKS cluster"
+  value       = azurerm_kubernetes_cluster.bookinfo.kube_config_raw
+  sensitive   = true
+}
+
+
+output "nsg_id" {
+  description = "ID of the network security group"
+  value       = azurerm_network_security_group.aks.id
+}
+
+output "cluster_name" {
+  description = "Name of the AKS cluster"
+  value       = azurerm_kubernetes_cluster.bookinfo.name
+}
+
+output "cluster_id" {
+  description = "ID of the AKS cluster"
+  value       = azurerm_kubernetes_cluster.bookinfo.id
+}
+
+output "cluster_fqdn" {
+  description = "FQDN of the AKS cluster"
+  value       = azurerm_kubernetes_cluster.bookinfo.fqdn
+}
+
+output "kubectl_config_command" {
+  description = "Command to configure kubectl"
+  value       = "az aks get-credentials --resource-group ${azurerm_resource_group.bookinfo.name} --name ${azurerm_kubernetes_cluster.bookinfo.name}"
 }
--- variables.tf
+++ variables.tf
@@ -10,6 +10,30 @@ variable "location" {
   default     = "North Europe"
 }
 
+variable "cluster_name" {
+  description = "Name of the AKS cluster"
+  type        = string
+  default     = "bookinfo-aks"
+}
+
+variable "kubernetes_version" {
+  description = "Kubernetes version for AKS"
+  type        = string
+  default     = "1.31.10"
+}
+
+variable "node_count" {
+  description = "Number of nodes in the AKS cluster"
+  type        = number
+  default     = 2
+}
+
+variable "node_vm_size" {
+  description = "VM size for AKS nodes"
+  type        = string
+  default     = "Standard_B2s"
+}
+
 variable "tags" {
   description = "Tags to apply to resources"
   type        = map(string)
@@ -18,10 +42,4 @@ variable "tags" {
     Project     = "bookinfo"
     ManagedBy   = "terraform"
   }
-}
-
-variable "cluster_name" {
-  description = "Name of the AKS cluster"
-  type        = string
-  default     = "bookinfo-aks"
 }
