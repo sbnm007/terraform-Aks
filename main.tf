780d7705d054510803e2131857b2d43687f5bba2
sbnm007 <sibin.georgge@gmail.com>
2025-10-19 01:07:56 +0100

    Added Security Groups, VPC and Subnet

--- main.tf
+++ main.tf
@@ -1,3 +1,4 @@
+#Provider Configuration
 terraform {
   required_providers {
     azurerm = {
@@ -43,10 +44,38 @@ resource "azurerm_subnet" "aks" {
 }
 
 
+# Network Security Group and rule for AKS
+resource "azurerm_network_security_group" "aks" {
+  name                = "${var.cluster_name}-nsg"
+  location            = azurerm_resource_group.bookinfo.location
+  resource_group_name = azurerm_resource_group.bookinfo.name
 
+  security_rule {
+    name                       = "AllowHTTP"
+    priority                   = 1001
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "80"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
 
+  security_rule {
+    name                       = "AllowHTTPS"
+    priority                   = 1002
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "443"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
 
-
+  tags = var.tags
+}
 
 
 
